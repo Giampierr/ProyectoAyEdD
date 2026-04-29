e0bfe8909591e9649255ca81b8b7e747722c7876
AngeloPc <angelomelo1999@gmail.com>
2026-04-29 01:16:56 -0500

    fix (Main):Arregla el do vista

--- src/main/java/tienda/Main.java
+++ src/main/java/tienda/Main.java
@@ -25,125 +25,128 @@ public class Main {
 
         int miEntrada = -1;
         int miVista = -1;
-        System.out.println("Ingrese  a la vista ");
-        System.out.println("0)  Salir");
-        System.out.println("1)  Dueño");
-        System.out.println("2)  Vendedor");
-        miVista = miScanner.nextInt();
-        miScanner.nextLine();
-        try{
-            switch (miVista){
-                case 1:
-                    do {
-                        System.out.println(miInventario.alertar());
-                        System.out.println("\n0) Salir");
-                        System.out.println("1) Añadir Producto");
-                        System.out.println("2) Listar Inventario");
-                        System.out.println("3) Buscar por nombre");
-                        System.out.println("4) Buscar por id");
-                        System.out.println("5) Ordenar por nombre");
-                        System.out.println("6) Ordenar por precio");
 
-                        try {
+        do {
+            System.out.println("Ingrese  a la vista ");
+            System.out.println("0)  Salir");
+            System.out.println("1)  Dueño");
+            System.out.println("2)  Vendedor");
+            miVista = miScanner.nextInt();
+            miScanner.nextLine();
+            try{
+                switch (miVista){
+                    case 1:
+                        do {
+                            System.out.println(miInventario.alertar());
+                            System.out.println("\n0) Salir");
+                            System.out.println("1) Añadir Producto");
+                            System.out.println("2) Listar Inventario");
+                            System.out.println("3) Buscar por nombre");
+                            System.out.println("4) Buscar por id");
+                            System.out.println("5) Ordenar por nombre");
+                            System.out.println("6) Ordenar por precio");
 
-                            System.out.print("Ingrese opción: ");
-                            miEntrada = miScanner.nextInt();
-                            miScanner.nextLine();
+                            try {
 
-                            switch (miEntrada) {
+                                System.out.print("Ingrese opción: ");
+                                miEntrada = miScanner.nextInt();
+                                miScanner.nextLine();
 
-                                case 1:
+                                switch (miEntrada) {
 
-                                    try {
-                                        System.out.print("Nombre: ");
+                                    case 1:
+
+                                        try {
+                                            System.out.print("Nombre: ");
+                                            nombre = miScanner.nextLine();
+
+                                            System.out.print("Precio: ");
+                                            precio = miScanner.nextDouble();
+
+                                            System.out.print("Stock: ");
+                                            stock = miScanner.nextInt();
+                                            miScanner.nextLine();
+
+                                            Producto producto =
+                                                    new Producto(nombre, precio, stock);
+
+                                            miInventario.guardar(producto);
+
+                                            System.out.println("Producto guardado.");
+
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("Precio o stock inválido.");
+                                            miScanner.nextLine();
+                                        }
+
+                                        break;
+
+                                    case 2:
+                                        System.out.println(miInventario.listarProductos());
+                                        break;
+
+                                    case 3:
+                                        System.out.print("Nombre a buscar: ");
                                         nombre = miScanner.nextLine();
-
-                                        System.out.print("Precio: ");
-                                        precio = miScanner.nextDouble();
-
-                                        System.out.print("Stock: ");
-                                        stock = miScanner.nextInt();
-                                        miScanner.nextLine();
-
-                                        Producto producto =
-                                                new Producto(nombre, precio, stock);
-
-                                        miInventario.guardar(producto);
-
-                                        System.out.println("Producto guardado.");
-
-                                    } catch (InputMismatchException e) {
-                                        System.out.println("Precio o stock inválido.");
-                                        miScanner.nextLine();
-                                    }
-
-                                    break;
-
-                                case 2:
-                                    System.out.println(miInventario.listarProductos());
-                                    break;
-
-                                case 3:
-                                    System.out.print("Nombre a buscar: ");
-                                    nombre = miScanner.nextLine();
-                                    System.out.println(
-                                            miInventario.busquedaLineal(nombre)
-                                    );
-                                    break;
-
-                                case 4:
-
-                                    try {
-                                        System.out.print("ID a buscar: ");
-                                        id = miScanner.nextInt();
-                                        miScanner.nextLine();
-
                                         System.out.println(
-                                                miInventario.busquedaLinealId(id)
+                                                miInventario.busquedaLineal(nombre)
                                         );
+                                        break;
 
-                                    } catch (InputMismatchException e) {
-                                        System.out.println("ID inválido.");
-                                        miScanner.nextLine();
-                                    }
+                                    case 4:
 
-                                    break;
+                                        try {
+                                            System.out.print("ID a buscar: ");
+                                            id = miScanner.nextInt();
+                                            miScanner.nextLine();
 
-                                case 5:
-                                    miInventario.ordenarPorNombre();
-                                    System.out.println("Ordenado por nombre.");
-                                    System.out.println(miInventario.listarProductos());
-                                    break;
+                                            System.out.println(
+                                                    miInventario.busquedaLinealId(id)
+                                            );
 
-                                case 6:
-                                    miInventario.ordenarPorPrecio();
-                                    System.out.println("Ordenado por precio.");
-                                    System.out.println(miInventario.listarProductos());
-                                    break;
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("ID inválido.");
+                                            miScanner.nextLine();
+                                        }
 
-                                case 0:
-                                    System.out.println("Saliendo...");
-                                    break;
+                                        break;
 
-                                default:
-                                    System.out.println("Opción inválida.");
+                                    case 5:
+                                        miInventario.ordenarPorNombre();
+                                        System.out.println("Ordenado por nombre.");
+                                        System.out.println(miInventario.listarProductos());
+                                        break;
+
+                                    case 6:
+                                        miInventario.ordenarPorPrecio();
+                                        System.out.println("Ordenado por precio.");
+                                        System.out.println(miInventario.listarProductos());
+                                        break;
+
+                                    case 0:
+                                        System.out.println("Saliendo...");
+                                        break;
+
+                                    default:
+                                        System.out.println("Opción inválida.");
+                                }
+
+                            } catch (InputMismatchException e) {
+                                System.out.println("Debes ingresar un número.");
+                                miScanner.nextLine();
                             }
 
-                        } catch (InputMismatchException e) {
-                            System.out.println("Debes ingresar un número.");
-                            miScanner.nextLine();
-                        }
+                        } while (miEntrada != 0);
 
-                    } while (miEntrada != 0);
-                    break;
-                case 2:
-                    //Por hacer
+                        break;
+                    case 2:
+                        System.out.println("Aun por hacer");
+                }
+            }catch (InputMismatchException e){
+                System.out.printf("Ingrese un valor valido");
             }
-        }catch (InputMismatchException e){
-            System.out.printf("Ingrese un valor valido");
-        }
-
+        }while(miVista !=0);
         miScanner.close();
-        System.out.println("Gracias.");
+        System.out.println("Saliendo.");
     }
 }
