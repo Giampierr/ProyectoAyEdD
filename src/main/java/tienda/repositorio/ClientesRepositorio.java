b488777f1a8959b999ebbbec23b430389e295fbb
AngeloPc <angelomelo1999@gmail.com>
2026-04-29 13:57:16 -0500

    feat (ClientesRepositorio) : agrega metodos de buscar por nombre y buscar por dni

--- src/main/java/tienda/repositorio/ClientesRepositorio.java
+++ src/main/java/tienda/repositorio/ClientesRepositorio.java
@@ -1,7 +1,35 @@
 package tienda.repositorio;
-
 import tienda.modelo.Cliente;
+import tienda.modelo.Producto;
+
+import java.util.List;
 
 public class ClientesRepositorio {
+    private List<Cliente> clientes;
 
+    public ClientesRepositorio(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public void guardar(Cliente misClientes) {
+        clientes.add(misClientes);
+    }
+
+    public String busquedaLinea(String nombreBuscado) {
+        for (Cliente cliente : clientes) {
+            if (cliente.getNombre().equalsIgnoreCase(nombreBuscado)) {
+                return cliente.mostrar();
+            }
+        }
+        return "No se encontro el cliente";
+    }
+
+    public String busquedaDni(String dniBuscado) {
+        for (Cliente cliente : clientes) {
+            if (cliente.getDni().equalsIgnoreCase(dniBuscado)) {
+                return cliente.mostrar();
+            }
+        }
+        return "No se encontro el cliente ";
+    }
 }
